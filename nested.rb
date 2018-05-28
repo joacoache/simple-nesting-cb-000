29cfd23a8ad400364c33e5e6a252a8b343aba51e
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-28 21:04:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][language].to_s
+		 programmer_hash[:dennis_ritchie][language][0]
 end
 
 def adding_matz
@@ -83,7 +83,7 @@ def adding_matz
      }
 
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-	
+
   programmer_hash
 end
 
